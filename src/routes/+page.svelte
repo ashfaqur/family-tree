fb56ea11e4681cad8b78ee2cc62b3d55181639e8
Ashfaqur Rahman <ashfaqur.rahman1@gmail.com>
2024-01-10 09:55:38 +0100

    wip

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,37 @@
+<script lang="ts">
+    import FamilyChart from "family-chart";
+    import { onMount, onDestroy } from "svelte";
+
+    let chart;
+
+    onMount(() => {
+        console.log("Creating family chart");
+        chart = new FamilyChart({
+            container: "#chart",
+            data: {
+                name: "John Doe",
+                children: [
+                    {
+                        name: "Jane Doe",
+                        children: [
+                            { name: "Alice Doe" },
+                            { name: "Bob Doe" },
+                            { name: "Charlie Doe" },
+                        ],
+                    },
+                    {
+                        name: "Jim Doe",
+                        children: [{ name: "David Doe" }, { name: "Eve Doe" }],
+                    },
+                ],
+            },
+        });
+    });
+</script>
+
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>
+    Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
+
+<div id="chart"></div>
--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,11 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	resolve: {
+		alias: {
+		  'family-chart': 'node_modules/family-chart/dist/family-chart.js',
+		},
+	  },
 });
+
