f269615bd5e3718116453d25738a70ac0240802c
Ashfaqur Rahman <ashfaqur.rahman1@gmail.com>
2024-01-11 10:18:33 +0100

    tailwind installed

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-    import "./family-chart.css";
+    import "../app.css";
 </script>
 
 <slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,5 @@
-<h1>Family Chart Landing Page</h1>
+<h1 class="text-green-400">Family Chart Landing Page</h1>
+
+<p class="text-red-200">Take care</p>
+
+<h1 class="text-3xl font-bold underline">Hello world!</h1>
