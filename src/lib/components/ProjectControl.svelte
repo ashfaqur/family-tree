cdf7f25d6cbd61db397a819c5475bc7166cae29e
Ashfaqur Rahman <ashfaqur.rahman1@gmail.com>
2024-11-08 01:34:41 +0100

    dropdown fully working as expected

--- src/lib/components/ProjectControl.svelte
+++ src/lib/components/ProjectControl.svelte
@@ -3,23 +3,46 @@
   import EditIcon from "$lib/components/svg/EditIcon.svelte";
   import CreateIcon from "$lib/components/svg/CreateIcon.svelte";
   import SaveIcon from "$lib/components/svg/SaveIcon.svelte";
+  import { onMount } from "svelte";
 
   export let projects: string[] = ["Default", "New"];
+  export let selectedProject: string = "Default";
+
+  let dropdownElement: HTMLElement;
+
+  function selectItem(item: string) {
+    selectedProject = item;
+    // Close the dropdown by removing the focus
+    if (dropdownElement) {
+      const activeElement = document.activeElement as HTMLElement;
+      if (activeElement) {
+        activeElement.blur();
+      }
+      dropdownElement.blur();
+    }
+  }
 </script>
 
 {#if $user}
   <div class="flex flex-row justify-start items-center space-x-1 bg-base-100">
     <h2>Projects:</h2>
-    <div class="dropdown">
-      <div tabindex="0" role="button" class="btn m-1">
-        {projects ? projects[0] : ""}
+    <div class="dropdown" bind:this={dropdownElement}>
+      <div
+        tabindex="0"
+        role="button"
+        class="btn m-1 min-w-52 flex justify-start hover:bg-base-200"
+      >
+        {selectedProject}
       </div>
       <ul
         tabindex="0"
-        class="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow"
+        class="dropdown-content menu bg-base-100 rounded-box z-30 w-52 p-2 shadow"
       >
-        <li><a>Item 1</a></li>
-        <li><a>Item 2</a></li>
+        {#each projects as item}
+          <li>
+            <a on:click|preventDefault={() => selectItem(item)}>{item}</a>
+          </li>
+        {/each}
       </ul>
     </div>
     <button class="btn min-h-10 h-10"
